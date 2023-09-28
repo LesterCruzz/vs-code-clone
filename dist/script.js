6202342b0c3b6e4e090d77743c01c0faad784b19
unknown <cpe.cruz.marklester@gmail.com>
2023-09-28 09:18:00 +0800

    Natangke ni sir

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,25 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY >= 100) {
+  //   header.style="background:pink; position:fixed; width:100%; z-index:999;"
+  header.classList.add("blue")
+} else {
+  //   header.style="background:#2b2b2b;";
+  header.classList.remove("blue")
+  }
+});
+
+
 const toggleBtn = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__nav");
-
 
 // toggleCls.addEventListener("click", () => {
 //   toggleBtn.classList.remove("open");
 //   headerNav.classList.remove("open");
 // });
 toggleBtn.addEventListener("click", () => {
-  
   toggleBtn.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
@@ -16,7 +28,14 @@ const toggleChev = document.querySelector(".chevron");
 const toggleDrop = document.querySelector(".dropdown");
 
 toggleChev.addEventListener("click", () => {
-  
   toggleChev.classList.toggle("open");
   toggleDrop.classList.toggle("open");
+});
+
+const toggleChevRight = document.querySelector(".chevron");
+const tweetPage = document.querySelector(".tweet__page_2");
+
+toggleChevRight.addEventListener("click", () => {
+  toggleChevRight.classList.toggle("open");
+  tweetPage.classList.toggle("open");
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,19 @@ module.exports = {
         'light': '#ffffff',
         'll-light': '#f5f5f5',
         'l-light': '#e3e3e3',
+        'll-gray': '#474848',
+        'l-gray': '#3d3d42',
+        'gray': '#dededf',
         'l-dark': '#2c2c32',
         'dark': '#25252b ',
         'primary': '#0066b8',
       },
       fontFamily: {
        'Montserrat': 'sans-serif',
-      }
+      },
+      // backgroundImage: {
+      //   'twitter': "url(../im)"
+      // }
     },
   },
   plugins: [],
